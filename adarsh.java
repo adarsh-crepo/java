407b945903f79b901340c63fee7fb9e85bc43d4f
Kumar Adarsh <adarsh3128@gmail.com>
2024-12-29 20:35:02 +0530

    Update adarsh.java

--- adarsh.java
+++ adarsh.java
@@ -1,5 +1,5 @@
 public class adarsh{
 	public static void main(String[] args){
-		System.out.println("Hello Everyone!, It's a bring morning!");
+		System.out.println("Hello Everyone!,I am Kumar Adarsh!");
 						}
 		}
